3535de8ad92c6dff02e5df9950ab6aa693a30e59
Christopher - RtF <58520035+christopher-rtf@users.noreply.github.com>
2023-11-02 01:58:24 -0400

    Add Process classes; increase version to 1.8.1

---
+++ Process/Process.cs
@@ -0,0 +1,48 @@
+﻿// Copyright 2022-2023 Raising the Floor - US, Inc.
+//
+// Licensed under the New BSD license. You may not use this file except in
+// compliance with this License.
+//
+// You may obtain a copy of the License at
+// https://github.com/raisingthefloor/morphic-windowsnative-lib-cs/blob/main/LICENSE
+//
+// The R&D leading to these results received funding from the:
+// * Rehabilitation Services Administration, US Dept. of Education under
+//   grant H421A150006 (APCP)
+// * National Institute on Disability, Independent Living, and
+//   Rehabilitation Research (NIDILRR)
+// * Administration for Independent Living & Dept. of Education under grants
+//   H133E080022 (RERC-IT) and H133E130028/90RE5003-01-00 (UIITA-RERC)
+// * European Union's Seventh Framework Programme (FP7/2007-2013) grant
+//   agreement nos. 289016 (Cloud4all) and 610510 (Prosperity4All)
+// * William and Flora Hewlett Foundation
+// * Ontario Ministry of Research and Innovation
+// * Canadian Foundation for Innovation
+// * Adobe Foundation
+// * Consumer Electronics Association Foundation
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morphic.WindowsNative.Process;
+
+public class Process
+{
+     public static string[] GetCurrentProcessNames()
+     {
+          List<string> result = new();
+
+          var processes = System.Diagnostics.Process.GetProcesses();
+          foreach (var process in processes)
+          {
+               // NOTE: we do not check process.HasExited here, as we should never get any exited processes in a fresh process list (and checking the property will consume unnecessary processor cycles)
+               var processName = process.ProcessName;
+               result.Add(processName);
+          }
+
+          return result.ToArray();
+     }
+}
